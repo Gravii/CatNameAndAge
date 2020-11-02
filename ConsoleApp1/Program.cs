413da1b3f3473b0afe083d04ff1965b41957d776
Gravii <uik.1.proc@gmail.com>
2020-11-02 13:37:14 +0300

    Я добавил хрень, которая, вроде, работает

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Рамис", new DateTime(2007, 6, 20));
+            Console.WriteLine($"Коту по имени {cat.Name} уже {cat.GetAge()} лет");
+        }
+    }
+}
