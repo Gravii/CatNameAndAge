b29e559d9e0b4e8ef8a633f0d91402ac265edf21
Gravii <uik.1.proc@gmail.com>
2020-11-06 13:30:02 +0300

    У кошек появился Вольер с автоматической подачей жрачки

--- ConsoleApp1/Cat.cs
+++ ConsoleApp1/Cat.cs
@@ -5,10 +5,13 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Cat
+    public class Cat
     {
+
         byte _hungryStatus;
+        public event EventHandler HungryStatusChanged;
         public Cat(string name, DateTime birthday)
+
         {
             Name = name;
             BirthDay = birthday;
@@ -17,38 +20,46 @@ namespace ConsoleApp1
 
         public string Name
         {
-            get; set;
+            get;
+            set;
         }
-
+        public void MakeNoise()
+        {
+            Console.WriteLine($"{Name } мяукает");
+        }
         public DateTime BirthDay
         {
             get; set;
         }
-
-        public void MakeNoise() => Console.WriteLine(Name + " мяукает");
-
         public int GetAge()
         {
             return (DateTime.Today - BirthDay).Days / 365;
         }
-
-        public event EventHandler HungryStatusChanged;
-        public void Feed() => HungryStatus = 100;
         public byte HungryStatus
         {
             get { return _hungryStatus; }
             set
             {
-                if (value < 0)
-                    _hungryStatus = 0;
-                else if (value > 100)
-                    _hungryStatus = 100;
+                byte status = value;
+                if (status < 0)
+                {
+                    status = 0;
+                }
+                else if (status > 100)
+                {
+                    status = 100;
+                }
                 else
                     _hungryStatus = value;
-
                 if (_hungryStatus != value)
-                    HungryStatusChanged?.Invoke(this, null); 
+                {
+                    HungryStatusChanged?.Invoke(this, null);
+                }
             }
+        }
+        public void Feed(byte needFood)
+        {
+            HungryStatus += needFood;
         }
 
         public void GetStatus()
@@ -82,13 +93,13 @@ namespace ConsoleApp1
             }
             Console.ResetColor();
         }
+
+
         async Task LifeCircle()
         {
-            await Task.Delay(1000);
+            await Task.Delay(100);
             HungryStatus -= 10;
             await LifeCircle();
         }
-
-        
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,39 +10,20 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Cat cat = new Cat("Рамис", new DateTime(2007, 6, 20));
+            cat.MakeNoise();
             Console.WriteLine($"Коту по имени {cat.Name} уже {cat.GetAge()} лет");
-
-            cat.HungryStatus = 150;
-            cat.HungryStatusChanged += Cat_HungryStatusChanged;
+            // cat.HungryStatus = 150;
 
             Cat cat2 = new Cat("Михалыч", new DateTime(2015, 6, 1));
+            cat2.MakeNoise();
             Console.WriteLine($"Коту по имени {cat2.Name} уже {cat2.GetAge()} лет");
-            cat2.HungryStatusChanged += Cat2_HungryStatusChanged;
-            // Я где-то часа два пытался понять, почему-то она криво работает, а вот почему.
-            // Так должно быть или нет?
+
+            CatSmartHouse catHouse = new CatSmartHouse(900);
+            catHouse.AddCat(cat);
+            catHouse.AddCat(cat2);
+
             Console.ReadLine();
         }
 
-        private static void Cat2_HungryStatusChanged(object sender, EventArgs e)
-        {
-            Random rnd = new Random();
-            Cat cat = (Cat)sender;
-            if (cat.HungryStatus < 20 && rnd.Next(0, 10) < 5)
-                cat.Feed();
-            else
-                cat.GetStatus();
-            // throw new NotImplementedException();
-        }
-
-        private static void Cat_HungryStatusChanged(object sender, EventArgs e)
-        {
-            Random rnd = new Random();
-            Cat cat = (Cat)sender;
-            if (cat.HungryStatus < 20 && rnd.Next(0, 10) < 5)
-                cat.Feed();
-            else
-                cat.GetStatus();
-            // throw new NotImplementedException();
-        }
     }
 }
